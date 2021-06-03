377485f930082d5bbba2a3666dadf606b3d95606
spartzames <spartzames@gmail.com>
2021-06-03 14:57:28 +0900

    숫자야구 hooks 전환

--- lecture/NumberBaseball.jsx
+++ lecture/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Try from './Try';
 
 function getNumbers () { // 숫자 네개를 중복없이 뽑는 함수
@@ -11,62 +11,49 @@ function getNumbers () { // 숫자 네개를 중복없이 뽑는 함수
     return array;
 };
 
-class NumberBaseball extends Component {
+const NumberBaseball = () => {
 
-    state = {
-        result: '',
-        value: '',
-        answer: getNumbers(), // ex: [1,3,5,7]
-        tries: [],
-    };
+    const [result, setResult] = useState('');
+    const [value, setValue] = useState('');
+    const [answer, setAnswer] = useState(getNumbers());
+    const [tries, setTries] = useState([]);
 
-    onSubmitForm = (e) => {
-        e.preventDefault();
-        if (this.state.value === this.state.answer.join('')) {
-            this.setState((prevState) => {
-                return {
-                    result: '홈런',
-                    tries: [...prevState.tries, {try: this.state.value, result:'홈런!'}], // 옛날 배열 복사해놓고 새로운 배열로 넣어줘야 react에서 서로 다름을 인식
-                    // react는 참조가 같으면 바뀐것을 인지하지 못한다. ex) this.state.tries.push(1); 바뀐것을 인지못함
-                }
-            });
-        } else {
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
-            let strike = 0;
-            let ball = 0;
-            if (this.state.tries.length >= 9) {
-                this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')} 였습니다.`
-                });
-                alert('게임을 다시 시작합니니다.');
-                this.setState({
-                    value: '',
-                    answer: getNumbers(),
-                    tries: [],
+    const onSubmitForm = (e) => {
+            e.preventDefault();
+            if (value === answer.join('')) {
+                setResult('홈런');
+                setTries((prevTries) => {
+                    return [...prevTries, {try: value, result:'홈런!'}];
                 });
             } else {
-                for (let i = 0; i < 4; i += 1) {
-                    if(answerArray[i] === this.state.answer[i]) {
-                        strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
-                        ball += 1;
+                const answerArray = value.split('').map((v) => parseInt(v));
+                let strike = 0;
+                let ball = 0;
+                if (tries.length >= 9) {
+                    setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다.`);
+                    alert('게임을 다시 시작합니니다.');
+                    setValue('');
+                    setAnswer(getNumbers());
+                    setTries([]);
+                } else {
+                    for (let i = 0; i < 4; i += 1) {
+                        if(answerArray[i] === answer[i]) {
+                            strike += 1;
+                        } else if (answer.includes(answerArray[i])) {
+                            ball += 1;
+                        }
                     }
+                    setTries((prevTries) => {
+                        return [...prevTries, { try: value, result: `${strike} 스트라이크 ${ball} 볼 입니다.`}];
+                    });
+                    setValue('');
                 }
-                this.setState((prevState) => {
-                    return {
-                        tries: [...prevState.tries, { try: this.state.value, result: `${strike} 스트라이크 ${ball} 볼 입니다.`}],
-                    }
-                });
             }
-        }
-    };
+        };
 
-    onChangeInput = (e) => {
-
-        this.setState({
-            value: e.target.value,
-        });
-    };
+    const onChangeInput = (e) => {
+            setValue(e.target.value);
+        };
 
     // fruits = [
     //     {fruit: '사과', taste: '맛없다'},
@@ -76,25 +63,22 @@ class NumberBaseball extends Component {
     //     {fruit: '포도', taste: '시다'},
     // ];
 
-    render() {
-        return (
-            <>
-                <h1>{this.state.result}</h1>
-                <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-                </form>
-                <div>시도: {this.state.tries.length}</div>
-                <ul>
-                    {this.state.tries.map((v, i) => {
-                        return (
-                            <Try key={`${i + 1 }차 시도 : `} tryInfo={v} index={i} /> // props
-                        );
-                    })}
-                </ul>
-            </>
-        );
-    }
-
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+                <input maxLength={4} value={value} onChange={onChangeInput} />
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v, i) => {
+                    return (
+                        <Try key={`${i + 1 }차 시도 : `} tryInfo={v} index={i} /> // props
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
 
 export default NumberBaseball; // import NumberBaseball;
--- lecture/Try.jsx
+++ lecture/Try.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Try extends Component {
+const Try = ({ tryInfo }) => {
 
-    render () {
-        return (
-            <li key={this.props.tryInfo.key}> {/*props가 있으면 parent가 있다고 인식*/}
-                <div>{this.props.tryInfo.try}</div>
-                <div>{this.props.tryInfo.result}</div>
-            </li>
-        );
-    }
-
+    return (
+        <li key={tryInfo.key}> {/*props가 있으면 parent가 있다고 인식*/}
+            <div>{tryInfo.try}</div>
+            <div>{tryInfo.result}</div>
+        </li>
+    );
 };
 
 export default Try;
