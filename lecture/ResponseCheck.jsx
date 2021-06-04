951d73dbab66390743b4dd603522017f52d49c6f
spartzames <spartzames@gmail.com>
2021-06-04 09:45:51 +0900

    hooks 전환

--- lecture/ResponseCheck.jsx
+++ lecture/ResponseCheck.jsx
@@ -1,81 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState, useRef } from 'react';
 
-class ResponseCheck extends Component {
-    state ={
-        state: 'waiting',
-        message: '클릭해서 시작하세요',
-        result: [],
-    };
+const ResponseCheck = () => {
+    // useState -> 값이 바뀌면 return 부분이 다시 rendering
+    // useRef -> 값이 바뀌어도 return 부분 rendering 하지 않음
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요');
+    const [result, setResult] = useState([]);
 
-    timeout;
-    startTime;
-    endTime;
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
 
-    onClickScreen = () => {
-        const { state, message, result } = this.state;
-        if (state === 'waiting') {
-            this.setState({
-                state: 'ready',
-                message: '초록색이되면 클릭하세요',
-            });
-            this.timeout = setTimeout(() => {
-                this.setState({
-                    state: 'now',
-                    message: '지금 클릭',
-                });
-                this.startTime = new Date();
-            }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 random);
-        } else if (state === 'ready') { // 성급하게 클릭
-            clearTimeout(this.timeout); // setTimeout 초기화
-            this.setState({
-                state: 'waiting',
-                message: '성급하시군요. 초록색이 되면 클릭하세요'
-            });
-        } else if (state === 'now') { // 반응속도 체크
-            this.endTime = new Date();
-            this.setState((prevState) => {
-                return {
-                    state: 'waiting',
-                    message: '클릭해서 시작하세요',
-                    result: [...prevState.result, this.endTime - this.startTime],
-                };
+    const onClickScreen = () => {
+        if(state === 'waiting') {
+            setState('ready');
+            setMessage('초록색이되면 클릭하세요');
+            timeout.current = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 random
+        } else if (state === 'ready') {
+            clearTimeout(timeout);
+            setState('waiting');
+            setMessage('성급하시군요. 초록색이 되면 클릭하세요.');
+        } else if (state === 'now') {
+            endTime.current = new Date();
+            setState('waiting');
+            setMessage('클릭해서 시작하세요.')
+            setResult((prevResult) => {
+                return (
+                    [...prevResult, endTime.current - startTime.current]
+                );
             });
         }
+    }
+
+    const onReset = () => {
+        setResult([]);
     };
 
-    onReset = () => {
-        this.setState({
-            result: [],
-        });
-    };
-
-    renderAverage = () => {
-        const { result } = this.state;
+    const renderAverage = () => {
         return (
-            result.length === 0
+            (result.length === 0) 
             ? null
             : <>
-                <div>평균시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-                <button onClick={this.onReset}>리셋</button>
+                <div>평균시간 : {result.reduce((a, c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
             </>
         );
     };
 
-    render() {
-        const { state, message } = this.state;
-        return (
-            <>
-                <div
-                id="screen"
-                className={state}
-                onClick={this.onClickScreen}
-                >
-                    {message}
-                </div>
-                {this.renderAverage()}
-            </>
-        );
-    }
+    return (
+        <>
+            <div
+            id="screen"
+            className={state}
+            onClick={onClickScreen}
+            >
+                {message}
+            </div>
+            {renderAverage()}
+        </>
+    );
 }
 
 export default ResponseCheck;
