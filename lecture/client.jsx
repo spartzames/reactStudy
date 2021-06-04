214ee590e06d24251c96eb511dd533697bf1631f
spartzames <spartzames@gmail.com>
2021-06-04 09:22:54 +0900

    반응속도체크

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NumberBaseball from './NumberBaseball';
 import RenderTest from './renderTest';
+import ResponseCheck from './ResponseCheck';
 
 // const React = require('react');
 // const ReactDom = require('react-dom');
@@ -9,6 +10,7 @@ import RenderTest from './renderTest';
 // const NumberBaseball = require('./NumberBaseball');
 
 
-ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
+// ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
 // ReactDOM.render(<RenderTest />, document.querySelector('#root'));
 // ReactDom.render(<WordRelay />, document.querySelector('#root'));
