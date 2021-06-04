9dba3da2e4a541c70dc0e44e5a1ae3e08c1c4be9
spartzames <spartzames@gmail.com>
2021-06-04 16:43:31 +0900

    로또추첨기 class

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -4,6 +4,7 @@ import NumberBaseball from './NumberBaseball';
 import RenderTest from './renderTest';
 import ResponseCheck from './ResponseCheck';
 import RSP from './RSP';
+import Lotto from './Lotto';
 
 // const React = require('react');
 // const ReactDom = require('react-dom');
@@ -11,7 +12,8 @@ import RSP from './RSP';
 // const NumberBaseball = require('./NumberBaseball');
 
 
-ReactDOM.render(<RSP />, document.querySelector('#root'));
+ReactDOM.render(<Lotto />, document.querySelector('#root'));
+// ReactDOM.render(<RSP />, document.querySelector('#root'));
 // ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
 // ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
 // ReactDOM.render(<RenderTest />, document.querySelector('#root'));
