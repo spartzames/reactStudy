095301285b5e468c7fd93ebb576b6adc38152af7
spartzames <spartzames@gmail.com>
2021-06-04 14:18:13 +0900

    가위바위보

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import NumberBaseball from './NumberBaseball';
 import RenderTest from './renderTest';
 import ResponseCheck from './ResponseCheck';
+import RSP from './RSP';
 
 // const React = require('react');
 // const ReactDom = require('react-dom');
@@ -10,7 +11,8 @@ import ResponseCheck from './ResponseCheck';
 // const NumberBaseball = require('./NumberBaseball');
 
 
-ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
+ReactDOM.render(<RSP />, document.querySelector('#root'));
+// ReactDOM.render(<ResponseCheck />, document.querySelector('#root'));
 // ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
 // ReactDOM.render(<RenderTest />, document.querySelector('#root'));
 // ReactDom.render(<WordRelay />, document.querySelector('#root'));
